4c937f4a165b052bf9424875419fc7a19da287ac
kirahxr <33960326+kirahxr@users.noreply.github.com>
2018-11-17 21:24:27 +0700

    Create 1baru.php

---
+++ 1baru.php
@@ -0,0 +1,19 @@
+<?php
+$yeah = array(
+
+		'itemId' => '12341822',
+		'itemName' => 'FGX Flannel Shirt',
+		'price' => 195000,
+		'availableColorAndSize' => array(
+			['color' => 'blue-black', 'size' => array('M','L','XL')], 
+			['color' => 'black-white', 'size' => 'L'], 
+		),
+		'freeShiping' => false,
+
+);
+//$json = var_dump($yeah);
+$json = json_encode($yeah)
+;echo $json;
+?>
+
+//baru paham :"
